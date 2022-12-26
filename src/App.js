96221a791ee5a7b8e4246968c98e108df593260a
hkelly098 <kehuang11@berkeley.edu>
2022-12-26 00:54:24 -0800

    First phase, brainstorm

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import { createTheme, ThemeProvider, makeStyles} from '@material-ui/core/styles';
+import {Typography} from '@material-ui/core'; 
+import NavBar from './components/NavBar';
+import Scheduler from './components/Scheduler';
 import './App.css';
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main:"#2e1667",
+    },
+    secondary: {
+      main:"#c7d8ed",
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Noto Nastaliq Urdu'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+      },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+});
+
+const styles = makeStyles({
+  wrapper: {
+    width: "65%",
+    margin: "auto",
+    textAlign: "center"
+  },
+  bigSpace: {
+    marginTop: "3rem"
+  },
+  littleSpace:{
+    marginTop: "2.5rem",
+  },
+})
+
 function App() {
+  const classes = styles(); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <NavBar/>
+        <div className={classes.wrapper}>
+          <Typography variant="h4" className={classes.bigSpace} color="primary">
+             Ochoa's Mobile Notary
+          </Typography>
+          <Typography variant="h5" className={classes.littleSpace} color="primary">
+            Ochoa's Mobile Notary is a trusted and reliable mobile notary business serving the city of Napa
+            and the surrounding areas. Our team of professional and certified notaries are available to travel to your home, 
+            office, or any location of your choice to notarize your documents. Whether you need a document notarized for a real estate transaction, a power of attorney, or any other legal matter, 
+            let the expertise at Ochoa's Mobile Notary handle it. 
+          </Typography>
+          <Typography variant="h5" className={classes.littleSpace} color="primary">
+            Schedule an appointment here or contact us at 415-123-4567 to take advantage of our convenient mobile notary services.
+          </Typography>
+        </div>
+
+        <div className={classes.wrapper}>
+          <Scheduler/>
+        </div>
+        
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/CustomBtn.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import {Button} from '@material-ui/core'
+import {withStyles} from '@material-ui/core/styles' //allows us to write css inside react component
+
+const StyledButton = withStyles({
+    root: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "44px",
+        padding: "0 55px",
+        boxSizing: "border-box",
+        borderRadius: 0, 
+        background: "#4f25f7",
+        color: "#fff",
+        transform: "none",
+        boxShadow: "6px 6px 0 0 #c7d8ed",
+        transition: "background .3s,border-color .3s,color .3s",
+        "&:hover": {
+            backgroundColor:  "#4f25f7"
+          },
+    },
+    label: {
+      textTransform: 'capitalize',
+    },
+  })(Button);
+
+export default function CustomBtn(props) {
+  return (
+    <StyledButton variant="contained">
+        {props.txt}
+    </StyledButton>
+  )
+}
